4530ab7902658353a4270d021c886d3e2f683bf4
Agenor Junior <agenor.dsjunior@gmail.com>
2025-06-04 10:12:46 -0400

    First Commit

---
+++ Timelicious/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timelicious
+//
+//  Created by Agenor Dionizio da Silva Junior on 2025-06-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
